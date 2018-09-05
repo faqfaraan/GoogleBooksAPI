4bd96b49c5e32330238cd8e47b1af750820812db
Faraan <faqfaraan@gmail.com>
2018-09-05 13:33:25 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'libre-service-testing';
+export class AppComponent implements OnInit{
+  isbn = '';
+  bookObj: any = '';
+
+  constructor( private http: HttpClient) {}
+
+  ngOnInit(){}
+
+  searchBook(){
+    const obs = this.http.get('https://www.googleapis.com/books/v1/volumes?q=isbn:' + this.isbn);
+    obs.subscribe((response) => console.log(response));
+  }
 }
